76945d8f81553640125b4bfb18c805bd466f1028
rohitinchale <cdac@dac-39.students.acts.in>
2024-07-31 15:52:25 +0530

    EvenOdd.java Added

---
+++ EvenOdd.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class EvenOdd {
+	public static void main(String[] args) {
+		int num;
+		for(num = 1; num < 21; num++) {
+			
+	       		if(num % 2 == 0) {
+				System.out.println(num + " is even number.");
+			}
+		}
+	}
+}	
+
